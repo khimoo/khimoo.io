22aa0b3937802f3f1c92de7baafd8003e07fbcae
khimoo <dailysentence@gmail.com>
2025-07-10 01:11:18 +0900

    routing & version

--- khimoo-portfolio/src/main.rs
+++ khimoo-portfolio/src/main.rs
@@ -1,5 +1,31 @@
 use khimoo_portfolio::App;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/admin")]
+    Admin,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {<App/>},
+        Route::Admin => html! { <h1> {"Admin"} </h1> },
+    }
+}
+
+#[function_component(Root)]
+fn root() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Root>::new().render();
 }
