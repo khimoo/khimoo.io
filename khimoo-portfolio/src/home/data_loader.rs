82b03d65062659a11d4322c93a93176a4c587dbc
khimoo <dailysentence1111@gmail.com>
2025-09-19 01:58:28 +0900

    test移動

--- khimoo-portfolio/src/home/data_loader.rs
+++ khimoo-portfolio/src/home/data_loader.rs
@@ -523,26 +523,3 @@ pub fn use_article_content(slug: Option<String>) -> (UseStateHandle<Option<Proce
 
     (data, loading, error)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_data_loader_creation() {
-        let loader = DataLoader::new();
-        assert_eq!(loader.base_url, "/data");
-    }
-
-    #[test]
-    fn test_data_loader_with_custom_base_url() {
-        let loader = DataLoader::with_base_url("/custom/path".to_string());
-        assert_eq!(loader.base_url, "/custom/path");
-    }
-
-    #[test]
-    fn test_error_display() {
-        let error = DataLoadError::NetworkError("Test error".to_string());
-        assert_eq!(error.to_string(), "Network error: Test error");
-    }
-}
